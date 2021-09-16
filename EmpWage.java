db61311150c60c48e6dc553cea7faffe0464bb05
unknown <rajputnikita5666@gmail.com>
2021-09-16 14:41:37 +0530

    Welcome message added

---
+++ EmpWage.java
@@ -0,0 +1,11 @@
+package com.EmpWage;
+
+public class EmpWage {
+
+	public static void main(String[] args) {
+		// Welcome Message
+		System.out.println("WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM");
+
+	}
+
+}
