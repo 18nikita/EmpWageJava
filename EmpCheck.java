01a2eabdcd6dba7b08b100347e39ea27a7ca1c25
unknown <rajputnikita5666@gmail.com>
2021-09-16 14:54:45 +0530

    Added EmpCheck Program

---
+++ EmpCheck.java
@@ -0,0 +1,20 @@
+package com.EmpWage;
+
+public class EmpCheck {
+
+	public static void main(String[] args) {
+		// UC-1-Check Employee is present or Absent
+	
+		//Constants 
+		int IS_FULL_TIME=1;
+		
+		//Computation
+		double empCheck= Math.floor(Math.random() * 10 ) % 2;
+		
+		if(empCheck == IS_FULL_TIME)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
+	}
+
+}
